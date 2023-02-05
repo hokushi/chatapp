5cd0802e72ebccfe5a130df6ba886ffd2978099a
hokushi <hokushi97@gmail.com>
2023-02-05 20:45:51 +0900

    綺麗に訂正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,40 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+const MESSAGE_URL = "http://localhost:8000/chatapp/message";
+
+const MessageComponent = ({ sendthing, index, isMine }) => {
+  const floatStyle = isMine ? "float-right" : "float-left";
+  return (
+    <div className="overflow-hidden" key={index}>
+      {isMine || (
+        <div className="float-left mr-2">{sendthing.sendername_id}</div>
+      )}
+      <div
+        className={`rounded-t-md border-2 px-2 py-0 mt-1 text-base bg-gray-300 ${floatStyle}`}
+      >
+        {sendthing.message}
+      </div>
+      <div className={`${floatStyle} mt-5 text-gray-400 text-xs`}>
+        {sendthing.created_at[0]}:{sendthing.created_at[1]}
+      </div>
+    </div>
+  );
+};
+
 const Chat = () => {
   const [message, setMessage] = useState("");
   const [sendList, setSendList] = useState(undefined);
   //これはuseeffectの第二引数に渡す用
   const [renderAfterSend, setRenderAfterSend] = useState(false);
-  const [getMessage, setGetMessage] = useState(undefined);
-  const MessageURL = "http://localhost:8000/chatapp/get_message";
-  const SendURL = "http://localhost:8000/chatapp/create_message";
 
-  const send = () => {
+  const sendMessage = () => {
     if (!message) {
       return;
     } else {
-      const sendinformation = { chatappUser_id: 3, message: message };
+      const sendInformation = { chatappUser_id: 3, message: message };
       axios
-        .post(SendURL, sendinformation)
+        .post(MESSAGE_URL, sendInformation)
         .then((res) => {
           console.log(res);
           setRenderAfterSend(!renderAfterSend);
@@ -28,15 +46,8 @@ const Chat = () => {
   };
 
   useEffect(() => {
-    /*if (!localStorage.getItem("send")) {
-      return;
-    }
-    const existingmessage = JSON.parse(localStorage.getItem("send"));
-    if (existingmessage) {
-      setSendList(existingmessage);
-    }*/
     axios
-      .get(MessageURL)
+      .get(MESSAGE_URL)
       .then((res) => {
         setSendList(res.data);
         console.log(res.data);
@@ -67,52 +78,37 @@ const Chat = () => {
               },
               index: number
             ) => {
-              if (sendthing.sendername_id == 3) {
-                return (
-                  <div className="overflow-hidden">
-                    <div className="rounded-t-md border-2 px-2 py-0 mt-1 text-base bg-gray-300 float-right">
-                      {sendthing.message}
-                    </div>
-                    <div className="float-right mt-5 text-gray-400 text-xs">
-                      {sendthing.created_at[0]}:{sendthing.created_at[1]}
-                    </div>
-                  </div>
-                );
-              }
-              if (sendthing.sendername_id == 1) {
-                return (
-                  <div className="overflow-hidden">
-                    <div className="float-left">{sendthing.sendername_id}</div>
-                    <div className="rounded-t-md border-2 ml-4 px-2 py-0 mt-1 text-base float-left">
-                      {sendthing.message}
-                    </div>
-                    <div className="float-left mt-5 text-gray-400 text-xs">
-                      {sendthing.created_at[0]}:{sendthing.created_at[1]}
-                    </div>
-                  </div>
-                );
-              }
+              const isMine = sendthing.sendername_id == 3;
+              return (
+                <MessageComponent
+                  sendthing={sendthing}
+                  index={index}
+                  isMine={isMine}
+                />
+              );
             }
           )}
         </div>
         <div className="fixed bottom-0 w-full px-0 mx-auto mt-10 bg-slate-300">
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => {
-              setMessage(e.target.value);
-            }}
-            className="block w-full p-2 pl-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-700 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
-            placeholder="message"
-            required
-          />
-          <button
-            type="submit"
-            onClick={send}
-            className="absolute right-0 bottom-0 bg-slate-300 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-          >
-            送信
-          </button>
+          <form>
+            <input
+              type="text"
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+              className="block w-full p-2 pl-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-gray-700 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
+              placeholder="message"
+              required
+            />
+            <button
+              type="submit"
+              onClick={sendMessage}
+              className="absolute right-0 bottom-0 bg-slate-300 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            >
+              送信
+            </button>
+          </form>
         </div>
       </>
     );
