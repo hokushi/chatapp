17b5260f49414935f91566109df46b934e8fafa3
hokushi <hokushi97@gmail.com>
2023-03-02 14:49:29 +0900

    deo-checkの訂正

--- pages/demo-check.tsx
+++ pages/demo-check.tsx
@@ -1,14 +1,16 @@
-import React from "react";
 import Check from "../components/check";
+import React, { useState, useEffect } from "react";
 
 const DemoCheck = () => {
+  const [check, setCheck] = useState(false);
+
   const demo = () => {
     console.log("demo");
   };
 
   return (
     <>
-      <Check messageID={0} setterfunc={demo} />
+      <Check messageID={0} setterfunc={demo} setDeleteCheck={setCheck} />
     </>
   );
 };
