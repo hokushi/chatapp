a0392e5fec72948825d963f6676b3497a1b4c0e9
hokushi <hokushi97@gmail.com>
2023-03-08 01:58:56 +0900

    step5までまとめた

--- pages/react-basic.tsx
+++ pages/react-basic.tsx
@@ -1,45 +1,46 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import Link from "next/link";
-
-const Component = () => {
-  return <div className="mt-5">コンポーネント</div>;
-};
 
 const ReactCheck = () => {
   return (
     <>
       {/* useStateの基本 */}
       <div>
-        <div>
-          <span className="text-red-500 text-xl ">step1</span> <br />
+        <div className="mt-5">
+          <span className="text-red-500 text-xl mt-5">step1</span> <br />
           <Link href="react-basic/basic1" className="text-3xl">
             useStateとuseEffectについて
           </Link>
         </div>
-        <div>
-          <span className="text-red-500 text-xl ">step2</span> <br />
+        <div className="mt-5">
+          <span className="text-red-500 text-xl mt-5">step2</span> <br />
           <Link href="react-basic/basic2" className="text-3xl">
             axiosについて
           </Link>
         </div>
-        <div>
-          <span className="text-red-500 text-xl ">step3</span> <br />
+        <div className="mt-5">
+          {" "}
+          <span className="text-red-500 text-xl mt-5">step3</span> <br />
           <Link href="react-basic/basic3" className="text-3xl">
             Linkタグについて
             <br />
             クエリとパスも学ぶよ
           </Link>
         </div>
-        <div>
-          <span className="text-red-500 text-xl ">step4</span> <br />
+        <div className="mt-5">
+          {" "}
+          <span className="text-red-500 text-xl mt-5">step4</span> <br />
           <Link href="react-basic/basic4" className="text-3xl">
             mapの使い方
           </Link>
         </div>
+        <div className="mt-5">
+          <span className="text-red-500 text-xl">step5</span> <br />
+          <Link href="react-basic/basic5" className="text-3xl">
+            コンポーネントの使い方
+          </Link>
+        </div>
       </div>
-      {/* コンポーネントの基本 */}
-      <Component />
     </>
   );
 };
