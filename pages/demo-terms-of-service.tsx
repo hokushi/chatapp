12c25838789e09b76539735f43172f26d0b5b539
hokushi <hokushi97@gmail.com>
2023-03-19 01:24:41 +0900

    見た目部分の作成

---
+++ pages/demo-terms-of-service.tsx
@@ -0,0 +1,12 @@
+import TermsOfService from "../components/terms-of-service";
+import React, { useState, useEffect } from "react";
+
+const Terms = () => {
+  return (
+    <>
+      <TermsOfService />
+    </>
+  );
+};
+
+export default Terms;
